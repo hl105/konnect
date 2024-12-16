b31eb3bcf293c7c00414aed240216fc19e10e69a
Johanna <hl105@wellesley.edu>
2024-12-16 15:11:05 -0500

    500 error

--- client/App.vue
+++ client/App.vue
@@ -1,18 +1,16 @@
 <script setup lang="ts">
 import router from "@/router";
-import { useToastStore } from "@/stores/toast";
 import { useUserStore } from "@/stores/user";
 import { storeToRefs } from "pinia";
 import { computed, onBeforeMount } from "vue";
-import { RouterLink, RouterView, useRoute } from "vue-router";
+import { RouterView, useRoute } from "vue-router";
 
 const currentRoute = useRoute();
 const currentRouteName = computed(() => currentRoute.name);
 const userStore = useUserStore();
 const { isLoggedIn } = storeToRefs(userStore);
-const { toast } = storeToRefs(useToastStore());
 
-// Make sure to update the session before mounting the app in case the user is already logged in
+// Make sure  to update the session before mounting the app in case the user is already logged in
 onBeforeMount(async () => {
   try {
     await userStore.updateSession();
